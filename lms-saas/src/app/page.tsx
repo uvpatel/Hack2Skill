d63eeaf3569305caf2cf60aab43a25838cec405c
uvpatel <uvpatel7271@gmail.com>
2026-01-04 18:28:53 +0530

    added all the soded all the source code

---
+++ lms-saas/src/app/page.tsx
@@ -0,0 +1,51 @@
+import ComapanionsList from '@/components/ComapanionsList'
+import CompanionCard from '@/components/CompanionCard'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
+
+const Page = () => {
+  return (
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard 
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard 
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives and Integrals"
+          subject="Maths"
+          duration={30}
+          color="#e5doff"
+        />
+        <CompanionCard 
+          id="786"
+          name="Verba the Vocubalary Builder"
+          topic="Language"
+          subject="English"
+          duration={30}
+          color="#BDE7FF"
+        />
+        
+      </section>
+
+      <section className='home-section'>
+        <ComapanionsList 
+        title="Recently completed sessions"
+        companions={recentSessions}
+        classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA /> 
+      </section>
+    </main>
+  )
+}
+
+export default Page
---
+++ lms-saas/src/app/subscription/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {PricingTable} from "@clerk/nextjs"
+
+function Subscription() {
+  return (
+    <div>
+      <PricingTable />
+    </div>
+  )
+}
+
+export default Subscription
