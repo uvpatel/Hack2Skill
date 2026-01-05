93e7487998eeec57099e941edfeb00b827839ac2
uvpatel <uvpatel7271@gmail.com>
2026-01-05 14:42:38 +0530

    fix routing

--- lms/app/page.tsx
+++ lms/app/page.tsx
@@ -5,6 +5,9 @@ import {recentSessions} from "@/constants";
 import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
 import {getSubjectColor} from "@/lib/utils";
 
+
+export const dynamic = "force-dynamic";
+
 const Page = async () => {
     const companions = await getAllCompanions({ limit: 3 });
     const recentSessionsCompanions = await getRecentSessions(10);
